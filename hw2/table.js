cad8fb20727c3b9d9753741154d1ece6c5c7f1c5
Daria Mikhailova <physteshka@physteshka-osx.local>
2017-11-06 02:52:35 +0300

    hw2: added localStorage

--- hw2/table.js
+++ hw2/table.js
@@ -1,28 +1,38 @@
-var header=[];
 
+var header=[];
 for (var i = 65; i <= 90; i++) {
     header.push(String.fromCharCode(i));
 }
 var N=20;
 var expr = {};
+var etable;
+var new_table = function() {
+        etable = "<div class='Table1'> <table border='1' cellpadding='0' cellspacing='0' width='100%'>";
+        etable += "<th></th>"
 
-var output = function() {
-var myTable = "<div class='Table1'> <table border='1' cellpadding='0' cellspacing='0' width='100%'>";
-myTable += "<th></th>"
-for(var i=0; i<header.length; i++)
-{myTable += "<th>"+header[i]+"</th>";
+        for(var i=0; i<header.length; i++)
+        {etable += "<th>"+header[i]+"</th>";
+        }
+        etable += '</thead>'
+        etable +="<tbody align='center'>";
+        for(var i=0; i<N; i++)
+        {etable += "<tr><td width='3.5%'>"+(i+1)+"</td><td width='3.5%'>".repeat(header.length)+ "</td></tr>";
+        }
+        etable += "</tbody>";
+        etable += "</table></div>";
+        return etable;
 }
-myTable += '</thead>'
-myTable +="<tbody align='center'>";
-for(var i=0; i<N; i++)
-{myTable += "<tr><td width='3.5%'>"+(i+1)+"</td><td width='3.5%'>".repeat(header.length)+ "</td></tr>";
-}
-myTable += "</tbody>";
-myTable += "</table></div>";
-document.getElementById("etable").innerHTML = myTable;
-}
-output();
 
+var init = function() {
+    if (localStorage.length == 0) {
+        etable = new_table();
+    } else {
+        applySetting();
+    }
+    document.getElementById("etable").innerHTML = etable;
+}
+
+init();
 var ind;
 
 $(function()	{
@@ -52,14 +62,49 @@ $(function()	{
                 $(this).parent().empty().html(expr[ind]);
             }
         });
+        $(window).keydown(function(event){
+            //ловим событие нажатия клавиши
+            if(event.keyCode === 13) {	//если это Enter
+                $('#edit').blur();	//снимаем фокус с поля ввода
+            }
+        });
+
+        $("#save").click(function () {
+           setSettings();
+        });
+
+        $("#clear").click(function () {
+           clearSettings();
+           location.reload();
+        });
+
     });
 });
 
-$(window).keydown(function(event){
-    //ловим событие нажатия клавиши
-    if(event.keyCode === 13) {	//если это Enter
-        $('#edit').blur();	//снимаем фокус с поля ввода
+
+function applySetting() {
+    if (localStorage.length != 0) {
+        etable = JSON.parse(localStorage.getItem('etable'));
+        expr = JSON.parse(localStorage.getItem('expressions'));
     }
-});
+}
 
+function setSettings() {
+    if ('localStorage' in window && window['localStorage'] != null) {
+        try {
+                localStorage.setItem('etable', JSON.stringify($("#etable").html()));
+                localStorage.setItem('expressions', JSON.stringify(expr));
+            } catch (e) {
+                if (e == QUOTA_EXCEEDED_ERR) {
+                    alert('Переполнение хранилища!');
+                }
+            }
+    } else {
+        alert('Данные не сохранятся, ваш браузер не поддерживает Local storage');
+    }
+}
 
+function clearSettings() {
+        localStorage.removeItem('etable');
+        localStorage.removeItem('expressions');
+}
